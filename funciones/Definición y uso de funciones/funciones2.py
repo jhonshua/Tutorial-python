afaf3cb744033c01875de4db517163e857bc61b0
Julio-PC\Julio <julio.llinas@gmail.com>
2024-12-28 11:28:59 -0400

    update

---
+++ funciones/Definición y uso de funciones/funciones2.py
@@ -0,0 +1,9 @@
+def saludar(nombre):
+    print ("Hola",  nombre)
+    
+    
+saludar("Marko")
+
+#-------------------------------------------------------------
+
+
--- hola_mundo.py
+++ hola_mundo.py
@@ -1,7 +1,13 @@
 
 
-
-
-
 nombre = input("Por favor, ingresa tu nombre: ")
 print("Hola mundo, " + nombre + "!")
+
+#-------------------------------------------------------------
+
+def saludar(nombre):
+    print ("Hola",  nombre)
+
+
+    
+saludar("Marko")
