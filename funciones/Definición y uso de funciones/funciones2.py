e5fccf7a953b9386885d5de967caa20d6bdac2ed
Julio-PC\Julio <julio.llinas@gmail.com>
2024-12-28 11:43:36 -0400

    update

--- funciones/Definición y uso de funciones/funciones2.py
+++ funciones/Definición y uso de funciones/funciones2.py
@@ -6,4 +6,21 @@ saludar("Marko")
 
 #-------------------------------------------------------------
 
+#PESO kg / altura2
+
+def calcularIMC(peso, altura):
+    imc = peso  / (altura**2)
+    return imc
+
+print(calcularIMC(80, 1.80))
+
+#-------------------------------------------------------------
+#son metodos nativos de python que nos permiten buscvar el min y el max de una lista 
+
+def CalcularMaxMin(lista):
+    return max(lista), min(lista)
+
+valMax, valMin = CalcularMaxMin([5,6,8,23,9,10,1,56])
+
+print(valMax, valMin)
 
